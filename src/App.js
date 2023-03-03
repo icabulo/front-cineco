e979649b012a9f8765592f57d60ea53d44e2ddc7
Ivan <icabulo@gmail.com>
2023-03-02 23:19:37 -0500

    initial template setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Front-end Cinecolombia</h1>
+      <h2>Proyecto grupal</h2>
+      <h2>PLANTILLA BASE</h2>
     </div>
   );
 }
